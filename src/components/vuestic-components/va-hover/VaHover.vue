1218bd547def7dc83ee671598efe7f454d840a68
igor.prokofiev <igorprodev1@gmail.com>
2020-10-22 20:42:52 +0300

    fix/hover

--- src/components/vuestic-components/va-hover/VaHover.vue
+++ src/components/vuestic-components/va-hover/VaHover.vue
@@ -6,6 +6,7 @@ import { h } from 'vue'
 
 @Options({
   name: 'VaHover',
+  emits: ['update:modelValue'],
 })
 export default class VaHover extends Mixins(StatefulMixin) {
   @Prop({
@@ -13,13 +14,16 @@ export default class VaHover extends Mixins(StatefulMixin) {
     default: false,
   }) readonly disabled!: boolean
 
+  @Prop({
+    type: Boolean,
+    default: false,
+  }) modelValue!: boolean
+
   onMouseEnter () {
-    this.value = true
     this.$emit('update:modelValue', true)
   }
 
   onMouseLeave () {
-    this.value = false
     this.$emit('update:modelValue', false)
   }
 
@@ -28,24 +32,28 @@ export default class VaHover extends Mixins(StatefulMixin) {
       return
     }
 
-    const slot = (this.$slots as any).default({ hover: this.value })
+    const slot = (this.$slots as any).default({ hover: this.modelValue })
 
     if (!Array.isArray(slot) || !slot.length) {
       return
     }
 
-    if (!this.disabled) {
-      slot.forEach((element) => {
-        element.data = element.data || {}
-        // is used to bind listeners using vue native function
-        // @ts-ignore
-        this._g(element.data, {
-          mouseenter: this.onMouseEnter,
-          mouseleave: this.onMouseLeave,
-        })
-      })
-    }
-    return h('div', slot)
+    // if (!this.disabled) {
+    //   slot.forEach((element) => {
+    //     console.log('element')
+    //     element.data = element.data || {}
+    //     // is used to bind listeners using vue native function
+    //     // @ts-ignore
+    //     ;(this as any)._g(element.data, {
+    //       mouseenter: this.onMouseEnter,
+    //       mouseleave: this.onMouseLeave,
+    //     })
+    //   })
+    // }
+    return h('div', !this.disabled ? {
+      onMouseEnter: this.onMouseEnter,
+      onMouseLeave: this.onMouseLeave,
+    } : {}, slot)
   }
 }
 </script>
